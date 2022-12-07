1b54a59566a3b1d6a97abb7502e482e608a1fac9
brunamello <brunacrespomello@gmail.com>
2022-12-07 02:51:09 +0000

    02 - Validation and Keyboard
    We learned more app building skills with Flutter. Shall we review?
    
    In this class, you learned to:
    Avoid overflow error with SingleChildScrollView:
    We prevent the phone keyboard from hiding parts of the screen. As? We wrap the 'Container (responsável por toda tela) com um novo Widget: oSingleChildScrollView`. Thus, it is possible to scroll and navigate the screen, avoiding keyboard overlap.
    
    Ease the user experience with KeyBoardType:
    When the user fills in a “name” text field, it is best to invoke a letter (alphabetical) keyboard. With this logic in mind, we customized the keyboards called by each of the text fields: the “name” field calls a letter keyboard; the “difficulty” field invokes the numerical keyboard; finally, the “image” field calls a top keyboard for URLs.
    
    Prevent empty information from being submitted by the form:
    We have learned that it is bad practice to submit nulls in a form. We don't want to display, for example, a task with no title or other information missing. To avoid this, we use the validator- parameter of TextFormField. Inside the validator, we create a condition to check if the field contains information or a null.
    
    Monitor validations with the widget Form:
    We saw that validations need someone to take care of them. That's where the new widget comes in: Form! It is responsible for verifying validations in a simpler way.
    
    Show the user that the information was sent with the Snackbar:
    To give more clarity about the app, we created a bar at the bottom of the screen that informs the user that the information entered in the form is correct and being saved. A snackbarbelongs to Scaffoldand appears only when all validations succeed (and disappears automatically)!

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -15,6 +15,8 @@ class _FormScreenState extends State<FormScreen>
   TextEditingController difficultController = TextEditingController();
   TextEditingController imageController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -29,87 +31,127 @@ class _FormScreenState extends State<FormScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Task'),
-      ),
-      body: Center(
-        child: Container(
-          height: 750,
-          width: 375,
-          decoration: BoxDecoration(
-            color: Colors.black12,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(width: 3),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  onChanged: ((text) {
-                    setState(() {});
-                  }),
-                  controller: nameController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Name',
-                    fillColor: Colors.white70,
-                    filled: true,
-                  ),
-                ),
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('New Task'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 650,
+              width: 375,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 3),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: difficultController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Difficulty',
-                    fillColor: Colors.white70,
-                    filled: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
+                          return 'Insert Task Name';
+                        }
+                        return null;
+                      },
+                      onChanged: ((text) {
+                        setState(() {});
+                      }),
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Name',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: imageController,
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Image',
-                    fillColor: Colors.white70,
-                    filled: true,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) > 5 ||
+                            int.parse(value) < 1) {
+                          return 'Insert Difficult Level Between 1 and 5';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      controller: difficultController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Difficulty',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 72,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 2, color: Colors.blue),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    imageController.text,
-                    errorBuilder: (BuildContext context, Object exception,
-                        StackTrace? stackTrace) {
-                      return Image.asset('assets/images/no_image.webp');
-                    },
-                    fit: BoxFit.cover,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Insert one Image URL';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.url,
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Image',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
                   ),
-                ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder: (BuildContext context, Object exception,
+                            StackTrace? stackTrace) {
+                          return Image.asset('assets/images/no_image.webp');
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print(nameController.text);
+                          print(difficultController.text);
+                          print(imageController.text);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Saving new task'),
+                            ),
+                          );
+                        }
+                      },
+                      child: const Text('Add!')),
+                ],
               ),
-              ElevatedButton(onPressed: () {}, child: const Text('Add!')),
-            ],
+            ),
           ),
         ),
       ),
